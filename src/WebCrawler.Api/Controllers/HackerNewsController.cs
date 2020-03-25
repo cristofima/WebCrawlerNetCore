c3b18fb0b6470475bd1743816f08cc93811f82c0
Cristopher Coronado Moreira <cristofima@hotmail.com>
2020-03-24 23:40:02 -0500

    Hackers News API

---
+++ src/WebCrawler.Api/Controllers/HackerNewsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebCrawler.Core.Entities;
+using WebCrawler.Core.Interfaces;
+
+namespace WebCrawler.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HackerNewsController : ControllerBase
+    {
+        private readonly IHackerNewService _hackerNewService;
+
+        public HackerNewsController(IHackerNewService hackerNewService)
+        {
+            _hackerNewService = hackerNewService;
+        }
+
+        /// <summary>
+        /// Hacker News List
+        /// </summary>
+        [HttpGet]
+        public async Task<IEnumerable<HackerNew>> ListAsync()
+        {
+            var result = await _hackerNewService.FindAllAsync();
+            return result;
+        }
+    }
+}
